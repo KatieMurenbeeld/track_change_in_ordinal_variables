5e6bcde68e9b58152ef20316071a5f35581fddb3
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-08-01 15:55:44 -0600

    init commit

---
+++ scripts/test/00_load_data.R
@@ -0,0 +1,74 @@
+# ==============================================================================
+# Load the WVO Prediction Data and Create a New Table with Vector Coordinates
+# ==============================================================================
+
+# Load Required Packages
+library(tidyverse)  # For loading and manipulating data tables
+
+# STEP 1: Load, join, and filter the prediction data and the meta data
+# -------------------------------------------------------------
+# 1. Load the wildlife value orientation predictions
+
+df <- read_csv("/Users/katiemurenbeeld/Analysis/VIP_NLP/output/predictions/grizzly_bear_05_preds_gamma_2025-01-31.csv")
+meta_df <- read_csv("/Users/katiemurenbeeld/Analysis/VIP_NLP/data/original/metadata_w_coverage_type_gamma_0.5.csv")
+
+## 1.1 Join the two datasets by GOID = Article_ID
+
+df_join <- df %>% 
+  left_join(meta_df, by = c("Article_ID" = "GOID"))
+
+## 1.2 Filter for articles published by 
+## Great Falls Tribune, Publication ID = 43825
+## The Billings Gazette, Publication ID = 46179
+## The New York Times, Publication ID = 11561
+
+df_filt <- df_join %>%
+  filter(`Publication ID` %in% c(43825, 46179, 11561))
+
+# STEP 2. From predictions create vector points based on the magnitudes and directions
+# -------------------------------------------------------------
+# Where magnitude = monthly (or annual) count of articles with the same score 
+# And the angle is based on the predicted values (angle in degrees)
+# WVO = 1 = 270deg
+# WVO = 2 = 300deg
+# WVO = 3 = 330deg
+# WVO = 4 = 0deg
+# WVO = 5 = 30deg
+# WVO = 6 = 60deg
+# WVO = 7 = 90deg
+
+## 2.1 Adjust the dates to get months and years
+df_filt$Date.x <- as.Date(df_filt$Date.x) # make the date datetime
+
+df_time <- df_filt %>% # add columns for the month, year, and month-year
+  mutate(year = year(Date.x), 
+         month = month(Date.x),
+         month_yr = format(as.Date(Date.x), "%Y-%m"))
+
+df_time$month_yr <- as.Date(paste(df_time$month_yr, "-01", sep="")) # this needs to be in yr-mn-day for some reason so I just paste on a -01
+
+## 2.2 Aggregate the scores for each newspaper by month (or year)
+
+df_month <- df_time %>%
+  group_by(`Publication Title`, month_yr, reg_05_pred_class) %>%
+  summarise(mag = n())
+
+## 2.3 Add the degree direction 
+
+df_month <- df_month %>%
+  mutate(deg = case_when(reg_05_pred_class == 1 ~ 270,
+                         reg_05_pred_class == 2 ~ 300, 
+                         reg_05_pred_class == 3 ~ 330, 
+                         reg_05_pred_class == 4 ~ 360, 
+                         reg_05_pred_class == 5 ~ 30, 
+                         reg_05_pred_class == 6 ~ 60, 
+                         reg_05_pred_class == 7 ~ 90))
+
+## 2.4 Determine the x, y for each WVO vector based on the magnitude (mag) and degree (deg)
+
+df_month <- df_month %>%
+  mutate(y = mag * sin(deg*(pi/180)), 
+         x = mag * cos(deg*(pi/180)))
+
+
+
---
+++ scripts/test/01_practice_plotting_vectors.R
@@ -0,0 +1,183 @@
+# ==============================================================================
+# Test Plotting Vectors
+# ==============================================================================
+
+# Load Required Packages
+library(tidyverse) # For loading and manipulating data tables
+library(ggplot2) # For plotting and visualization
+
+# STEP 1: Load the WVO vector table 
+# -------------------------------------------------------------
+# 1. Load the wildlife value orientation vector coordinates from 00_load_data.R
+
+df_plot <- df_month
+
+# STEP 2: Plot two vectors with (0,0) as their origins
+# -------------------------------------------------------------
+# 2. Practice plotting two vectors
+
+ggplot() +
+  geom_point() +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[1], yend = df_plot$y[1]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[2], yend = df_plot$y[2]),
+               arrow = arrow()
+  ) +
+  xlim(-1,1) + 
+  ylim(-1,1) + 
+  theme_minimal()
+
+# STEP 3: Add the two vectors from Step 2 and plot 
+# -------------------------------------------------------------
+# 3.1 Vector addition
+
+y_new <- df_plot$y[1] + df_plot$y[2]
+x_new <- df_plot$x[1] + df_plot$x[2]
+
+ggplot() +
+  geom_point() +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[1], yend = df_plot$y[1]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[2], yend = df_plot$y[2]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = x_new, yend = y_new),
+               arrow = arrow(), color = "red"
+  ) +
+  xlim(-1,1) + 
+  ylim(-1,1) + 
+  theme_minimal()
+
+# STEP 4: The new vector from Step 3 is now the origin for the next month 
+# -------------------------------------------------------------
+# 4.1 I think I want the tail of the monthly vector to be at the head of the
+# previous monthly vector
+
+
+ggplot() +
+  geom_point() +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[1], yend = df_plot$y[1]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[2], yend = df_plot$y[2]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = x_new, yend = y_new),
+               arrow = arrow(), color = "red"
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[3], yend = df_plot$y[3]),
+               arrow = arrow(), color = "blue"
+  ) +
+  geom_segment(aes(x = x_new, y = y_new, xend = df_plot$x[3] + x_new, yend = df_plot$y[3] + y_new),
+               arrow = arrow(), color = "orange"
+  ) +
+  xlim(-3, 3) + 
+  ylim(-3, 3) + 
+  theme_minimal()
+
+# 4.2 Get the vector sum for each month-year and each news paper
+
+df_vec_sum <- df_plot %>%
+  group_by(`Publication Title`, month_yr) %>%
+  summarise(month_vec_x = sum(x), 
+            month_vec_y = sum(y))
+
+test <- df_vec_sum %>%
+  filter(`Publication Title` == "Great Falls Tribune") %>%
+  mutate(tail_x = 0, 
+         tail_y = 0,
+         head_x = month_vec_x,
+         head_y = month_vec_y)
+
+test$tail_x[2] <- test$head_x[1] 
+#test$tail_y[2] <- test$head_y[1]
+
+test$head_x[2] <- test$tail_x[2] + test$month_vec_x[2]
+#test$head_y[2] <- test$tail_y[2] + test$month_vec_y[2]
+
+#test$tail_x[3:nrow(test)] <- test$month_vec_x[3:nrow(test)] + test$tail_x[2:nrow(test)] 
+
+test_subset <- test[2:nrow(test),]
+
+#test_subset$tail_x[2:nrow(test_subset)] <- test_subset$month_vec_x[2:nrow(test_subset)] + test_subset$tail_x[1:nrow(test_subset)-1]
+
+for (i in 2:nrow(test_subset)){
+  test_subset$tail_x[i] <- test_subset$head_x[i-1]
+  #test_subset$tail_y[i] <- test_subset$head_y[i-1]
+  test_subset$head_x[i] <- test_subset$tail_x[i] + test_subset$month_vec_x[i]
+  test_subset$head_y[i] <- test_subset$tail_y[i] + test_subset$month_vec_y[i]
+}
+
+df_new <- rbind(test[1,], test_subset)
+
+#df_new <- df_new %>%
+#  mutate(end_x = month_vec_x + tail_x,
+#         end_y = month_vec_y + tail_y)
+
+
+ggplot() +
+  geom_point() +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[1], yend = df_plot$y[1]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[2], yend = df_plot$y[2]),
+               arrow = arrow()
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[1] + df_plot$x[2], yend = df_plot$y[1] + df_plot$y[2]),
+               arrow = arrow(), color = "red"
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_plot$x[3], yend = df_plot$y[3]),
+               arrow = arrow(), color = "blue", linetype = "dashed"
+  ) +
+  geom_segment(aes(x = df_plot$x[1] + df_plot$x[2], y = df_plot$y[1] + df_plot$y[2], 
+                   xend = df_plot$x[3] + df_plot$x[1] + df_plot$x[2], 
+                   yend = df_plot$y[3] + df_plot$y[1] + df_plot$y[2]),
+               arrow = arrow(), color = "blue"
+  ) +
+#  geom_segment(aes(x = 0, y = 0, xend = df_vec_sum$month_vec_x[3], yend = df_vec_sum$month_vec_y[3]),
+#               arrow = arrow(), color = "orange", linetype = "dashed"
+#  ) +
+  geom_segment(aes(x = df_plot$x[3] + df_plot$x[1] + df_plot$x[2], 
+                   y = df_plot$y[3] + df_plot$y[1] + df_plot$y[2], 
+                   xend = df_vec_sum$month_vec_x[3] + df_plot$x[3] + df_plot$x[2] + df_plot$x[1], 
+                   yend = df_vec_sum$month_vec_y[3] + df_plot$y[3] + df_plot$y[2] + df_plot$y[1]),
+               arrow = arrow(), color = "orange"
+  ) +
+#  geom_segment(aes(x = 0, y = 0, xend = df_vec_sum$month_vec_x[4], yend = df_vec_sum$month_vec_y[4]),
+#               arrow = arrow(), color = "purple", linetype = "dashed"
+#  ) +
+  geom_segment(aes(x = df_vec_sum$month_vec_x[3] + df_plot$x[3] + df_plot$x[2] + df_plot$x[1], 
+                   y = df_vec_sum$month_vec_y[3] + df_plot$y[3] + df_plot$y[2] + df_plot$y[1], 
+                   xend = df_vec_sum$month_vec_x[4]+ df_vec_sum$month_vec_x[3] + df_plot$x[3] + df_plot$x[2] + df_plot$x[1], 
+                   yend = df_vec_sum$month_vec_y[4] + df_vec_sum$month_vec_y[3] + df_plot$y[3] + df_plot$y[2] + df_plot$y[1]),
+               arrow = arrow(), color = "purple"
+  ) +
+#  geom_segment(aes(x = 0, y = 0, xend = df_vec_sum$month_vec_x[5]), yend = df_vec_sum$month_vec_y[5],
+#               arrow = arrow(), color = "forestgreen", linetype = "dashed"
+#  ) +
+  geom_segment(aes(x = 0, y = 0, xend = df_vec_sum$month_vec_x[5]), yend = df_vec_sum$month_vec_y[5],
+               arrow = arrow(), color = "forestgreen", linetype = "dashed"
+  ) +
+  geom_segment(aes(x = df_vec_sum$month_vec_x[4]+ df_vec_sum$month_vec_x[3] + df_plot$x[3] + df_plot$x[2] + df_plot$x[1], 
+                   y = df_vec_sum$month_vec_y[4] + df_vec_sum$month_vec_y[3] + df_plot$y[3] + df_plot$y[2] + df_plot$y[1], 
+                   xend = df_vec_sum$month_vec_x[5] + df_vec_sum$month_vec_x[4]+ df_vec_sum$month_vec_x[3] + df_plot$x[3] + df_plot$x[2] + df_plot$x[1], 
+                   yend = df_vec_sum$month_vec_y[5] + df_vec_sum$month_vec_y[4] + df_vec_sum$month_vec_y[3] + df_plot$y[3] + df_plot$y[2] + df_plot$y[1]),
+               arrow = arrow(), color = "forestgreen"
+  ) +
+  xlim(-8, 8) + 
+  ylim(-8, 8) + 
+  theme_minimal()
+
+ggplot(df_new, aes(head_x, head_y)) + 
+  geom_point()
+
+ggplot(df_vec_sum[1:4,], aes(month_vec_x, month_vec_y, color = month_yr)) +
+  geom_point()
+
+
+
+set.seed(111)
+df1 <- data.frame(WD=sample(10))
+df1$newColumn <- with(df1, c(WD[1],WD[-1]+WD[-nrow(df1)]))
