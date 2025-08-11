c1222422fb388190ada3f86b12d68ea68380ed86
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-08-11 13:04:09 -0600

    start of script

---
+++ scripts/test/01_practice_plotting_vectors_with_functions.R
@@ -0,0 +1,15 @@
+# ==============================================================================
+# Test Plotting Vectors
+# ==============================================================================
+
+# Load Required Packages
+library(tidyverse) # For loading and manipulating data tables
+library(ggplot2) # For plotting and visualization
+
+# STEP 1: Load the WVO vector tables and functions 
+# -------------------------------------------------------------
+# 1. Load the wildlife value orientation vector coordinates from 00_load_data.R
+
+df_month <- df_month
+df_year <- df_year
+
