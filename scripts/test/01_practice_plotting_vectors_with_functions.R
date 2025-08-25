fe8403e03e5626a1e0f132a0a8ffd58f6cfa25c5
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-08-25 16:28:35 -0600

    code attempting to use functions from my_functions.R to calculate the heads and tails of the aggregated vectors

--- scripts/test/01_practice_plotting_vectors_with_functions.R
+++ scripts/test/01_practice_plotting_vectors_with_functions.R
@@ -5,6 +5,7 @@
 # Load Required Packages
 library(tidyverse) # For loading and manipulating data tables
 library(ggplot2) # For plotting and visualization
+library(here) # Shorthand for calling absolute pathways
 
 # STEP 1: Load the WVO vector tables and functions 
 # -------------------------------------------------------------
@@ -12,4 +13,38 @@ library(ggplot2) # For plotting and visualization
 
 df_month <- df_month
 df_year <- df_year
+
+# 2. Load in the functions from /scripts/utilities/my_functions.R
+
+source(here::here("scripts/utilities/my_functions.R"))
+
+# STEP 2: Use the functions to create data frames of WVOs and publication of interest
+# -------------------------------------------------------------
+## Great Falls Tribune, Publication ID = 43825
+## The Billings Gazette, Publication ID = 46179
+## The New York Times, Publication ID = 11561
+
+# 2.1 Create a list of the publications
+publications <- c("Great Falls Tribune", "The Billings Gazette", "The New York Times")
+
+# 2.2 Create data frames for publication of interest at monthly and annual time steps
+
+df_month_plot <- vector_tails_to_head(data = df_month, time_step = month_yr, publication = publications[1])
+#df_month_plot <- vector_tails_to_zero(data = df_month, time_step = month_yr, publication = publications[1])
+
+#df_year_plot <- vector_tails_to_head(df_year, year, publications[1])
+df_year_plot <- vector_tails_to_zero(df_year, year, publications[1])
+
+
+ggplot(df_year_plot, aes(head_x, head_y)) + 
+  geom_line()
+
+ggplot(df_month_plot, aes(head_x, head_y)) + 
+  geom_line()
+
+
+
+
+
+
 
