c25679cb1338cbafd4b810e4690233fd20849f25
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-09-09 14:34:55 -0600

    renamed dataframe for time segments, made more for the 5 and 2 year steps, updated plot

--- scripts/test/_practice_trend_time_windows.R
+++ scripts/test/_practice_trend_time_windows.R
@@ -63,7 +63,7 @@ greys <- paletteer_c("ggthemes::Classic Gray", 30)
 
 ## set up data frames with x, y, endx, and endy
 
-gft_yr_df <- gfb_yr %>%
+gft_10yr_df <- gfb_yr %>%
   mutate(x_10 = year, 
          xend_10 = year+10, 
          y_10 = wm_wvo, 
@@ -71,82 +71,53 @@ gft_yr_df <- gfb_yr %>%
   filter(xend_10 <= max(year))
 
 ## Plot the line between the start and end year 10 year gap
-p10 <- ggplot(gft_yr_df, aes(x = x_10, y = y_10, xend = xend_10, yend = yend_10)) +
-  geom_segment(size = 1.5, arrow = arrow(length = unit(0.3, "cm")), color = test_colors[5]) + # Optional: adjust size and add arrows
-  gghighlight(x_10 %in% c(2004, 2007)) +
-  labs(title = "Multiple Line Segments",
-       x = "X-axis",
-       y = "Y-axis") +
-  theme_minimal()
+p10 <- ggplot(gft_10yr_df, aes(x = x_10, y = y_10, xend = xend_10, yend = yend_10)) +
+  geom_segment(size = 1, arrow = arrow(length = unit(0.15, "cm")), color = blues[30]) + # Optional: adjust size and add arrows
+  gghighlight(x_10 %in% c(2004, 2007))
+#the above code is adapted from the google AI result for "plot multiple geom_segments r"
 #This approach allows for the efficient plotting of numerous segments, with the ability to differentiate them by aesthetic mappings based on variables within the data frame.
-
-
-p10 <- ggplot(gfb_yr, aes(alpha = 0.5)) +
-  geom_point(aes(year, wm_wvo), color = "grey", alpha = 0.4) +
-  geom_segment(aes(x = year[1], y = wm_wvo[1], 
-               xend = year[9], yend = wm_wvo[9], alpha = 0.5),
-               arrow = arrow(length=unit(0.15,"cm")),
-               color = "#F49435FF", linewidth = 0.15
-  )
-print(p10)
-for (i in 1:nrow(gfb_yr)) {
-  p10 <- p10 + 
-   # geom_point(aes(x = i, y = gfb_yr$wm_wvo[gfb_yr$year == i]), color = "red") + 
-    #geom_point(aes(x = i + 10, y = gfb_yr$wm_wvo[gfb_yr$year == i + 10]), color = "blue") + 
-    geom_segment(x = gfb_yr$year[i], y = gfb_yr$wm_wvo[i], 
-                 xend = gfb_yr$year[i+10], yend = gfb_yr$wm_wvo[i+10],
-                 arrow = arrow(length=unit(0.15,"cm")), 
-                 color = "#F49435FF", linewidth = 0.15
-    ) + 
-    gghighlight(i %in% c(2004, 2007)) +
-  if (i > nrow(gfb_yr) - 10) {
-    break
-  }
-}
 print(p10)
 
 final_p10 <- p10 +
-  geom_point(x = 2004, y = gfb_yr$wm_wvo[gfb_yr$year == 2004], color = "#585858FF") +
-  geom_point(x = 2014, y = gfb_yr$wm_wvo[gfb_yr$year == 2014], color = "#585858FF") +
-  geom_point(x = 2007, y = gfb_yr$wm_wvo[gfb_yr$year == 2007], color = "#585858FF") +
-  geom_point(x = 2017, y = gfb_yr$wm_wvo[gfb_yr$year == 2017], color = "#585858FF") +
-  geom_segment(x = 2004, y = gfb_yr$wm_wvo[gfb_yr$year == 2004], 
-               xend = 2014, yend = gfb_yr$wm_wvo[gfb_yr$year == 2014],
-               arrow = arrow(length=unit(0.15,"cm")), , linewidth = 0.5,
-               color = "#225188FF") +
-  geom_segment(x = 2007, y = gfb_yr$wm_wvo[gfb_yr$year == 2007], 
-               xend = 2017, yend = gfb_yr$wm_wvo[gfb_yr$year == 2017],
-               arrow = arrow(length=unit(0.15,"cm")), linewidth = 0.5,
-               color = "#225188FF") +
+  geom_point(x = 2004, y = gft_10yr_df$y_10[gft_10yr_df$x_10 == 2004], color = "#585858FF") +
+  geom_point(x = 2014, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2014], color = "#585858FF") +
+  geom_point(x = 2007, y = gft_10yr_df$y_10[gft_10yr_df$x_10 == 2007], color = "#585858FF") +
+  geom_point(x = 2017, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2017], color = "#585858FF") +
   geom_hline(yintercept = 4, color = "grey", linetype = "dashed") + 
   annotate("text", x = 2027, y = 4.15, label = "Neutral") + 
   annotate("text", x = 2027, y = 6.1, label = "Mutualism") + 
   annotate("text", x = 2027, y = 1.65, label = "Domination") + 
-  annotate("text", x = 2014, y = gfb_yr$wm_wvo[gfb_yr$year == 2014] - 0.25, 
+  annotate("text", x = 2014, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2014] - 0.25, 
            label = "2014") +
-  annotate("text", x = 2002.7, y = gfb_yr$wm_wvo[gfb_yr$year == 2004], 
+  annotate("text", x = 2002.7, y = gft_10yr_df$y_10[gft_10yr_df$x_10 == 2004], 
            label = "2004") +
-  annotate("text", x = 2017, y = gfb_yr$wm_wvo[gfb_yr$year == 2017] + 0.25, 
+  annotate("text", x = 2017, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2017] + 0.25, 
            label = "2017") +
-  annotate("text", x = 2007, y = gfb_yr$wm_wvo[gfb_yr$year == 2007] - 0.25, 
+  annotate("text", x = 2007, y = gft_10yr_df$y_10[gft_10yr_df$x_10 == 2007] - 0.25, 
            label = "2007") +
-  annotate("richtext", x = 2017, y = gfb_yr$wm_wvo[gfb_yr$year == 2017] + 1.25, 
-           label = "<span style = 'font-size:10pt'> 2007 to 2017 shows a<br> shift towards <b>mutualism</b></span>", 
+  annotate("richtext", x = 2017, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2017] + 1.25, 
+           label = "<span style = 'font-size:10pt'> 2007 to 2017 shows a<br> 
+           shift towards <span style = 'color:#26456EFF'><b>mutualism</b></span>
+           </span>", 
            fill = NA, label.color = NA) +
-  annotate("richtext", x = 2014, y = gfb_yr$wm_wvo[gfb_yr$year == 2014] - 1, 
-           label = "<span style = 'font-size:10pt'> 2004 to 2014 shows a<br> shift towards <b>domination</b></span>", 
+  annotate("richtext", x = 2014, y = gft_10yr_df$yend_10[gft_10yr_df$xend_10 == 2014] - 1, 
+           label = "<span style = 'font-size:10pt'> 2004 to 2014 shows a<br> 
+           shift towards <span style = 'color:#26456EFF'><b>domination</b></span>
+           </span>", 
            fill = NA, label.color = NA) +
   annotate("richtext", x = 1998, y = 6.75, 
            label = "<span style = 'font-size:14pt'>Why choice of time frame 
            matters</span><br>
-       <span style = 'font-size:10pt'>the choice of the beginning and end data 
+       <span style = 'font-size:10pt'>the beginning and end data 
        points will<br> reflect different shifts in <b>W</b>ildlife <b>V</b>alue 
        <b>O</b>rientations</span>", 
            fill = NA, label.color = NA, hjust = 0) +
   ylim(1,7) +
   xlim(1998,2028) +
   labs(x = "<b>Year</b>", 
-       y = "<b>Weighted Mean WVO</b>") + 
+       y = "<b>Weighted Mean WVO</b>", 
+       caption = "Any combination of<span style = 'color:#26456EFF'>
+       <b>highlighed</b></span> segments <br> would show...</span>") + 
   theme_classic() +
   theme(title = element_markdown(vjust = -5),
         axis.line=element_blank(),
@@ -155,41 +126,34 @@ final_p10 <- p10 +
         axis.title.y = element_markdown(),
         axis.text.x = element_markdown(size = 11),
         axis.title.x = element_markdown(), 
-        legend.position = "none")
+        legend.position = "none", 
+        plot.caption = element_markdown(lineheight = 1.2))
 
 print(final_p10)
-ggsave(here::here("outputs/test/gft_10yr_wvo_shift.png"), final_p10, 
+ggsave(here::here("outputs/test/gft_10yr_wvo_shift_blues.png"), final_p10, 
        height = 6, width = 8, dpi = 300)
 
 ## Plot the line between the start and end year 5 year gap
-p5 <- ggplot(gfb_yr) +
-  geom_point(aes(year, wm_wvo)) +
-  geom_segment(x = 1999, y = gfb_yr$wm_wvo[gfb_yr$year == 1999], 
-               xend = 2004, yend = gfb_yr$wm_wvo[gfb_yr$year == 2004],
-               arrow = arrow(length=unit(0.15,"cm"))
-  )
-for (i in 1:nrow(gfb_yr)) {
-  p5 <- p5 + 
-    # geom_point(aes(x = i, y = gfb_yr$wm_wvo[gfb_yr$year == i]), color = "red") + 
-    #geom_point(aes(x = i + 10, y = gfb_yr$wm_wvo[gfb_yr$year == i + 10]), color = "blue") + 
-    geom_segment(x = gfb_yr$year[i], y = gfb_yr$wm_wvo[i], 
-                 xend = gfb_yr$year[i+5], yend = gfb_yr$wm_wvo[i+5],
-                 arrow = arrow(length=unit(0.15,"cm")),
-                 color = test_colors[i]
-    )
-  if (i > nrow(gfb_yr) - 5) {
-    break
-  }
-}
+## set up data frames with x, y, endx, and endy
 
-final_p5 <- p5 + 
-  geom_hline(yintercept = 4) + 
-  ylim(1,7) +
-  theme_bw()
+gft_5yr_df <- gfb_yr %>%
+  mutate(x_5 = year, 
+         xend_5 = year+5, 
+         y_5 = wm_wvo, 
+         yend_5 = lead(wm_wvo, n = 5)) %>%
+  filter(xend_5 <= max(year))
 
-print(final_p5)
+
 
 ## Plot the line between the start and end year 2 year gap
+gft_2yr_df <- gfb_yr %>%
+  mutate(x_2 = year, 
+         xend_2 = year+2, 
+         y_2 = wm_wvo, 
+         yend_2 = lead(wm_wvo, n = 2)) %>%
+  filter(xend_2 <= max(year))
+
+
 p2 <- ggplot(gfb_yr) +
   geom_point(aes(year, wm_wvo)) +
   geom_segment(x = 1999, y = gfb_yr$wm_wvo[gfb_yr$year == 1999], 
